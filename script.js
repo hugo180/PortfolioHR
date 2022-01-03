c7cd4bdf85684b3da0fb9f1713fd28c221b284a7
hugo180 <hugorubio180@gmail.com>
2022-01-03 00:03:25 -0700

    uploaded my Portfolio files

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+        $('.navbar').addClass("sticky");
+    }else{   
+        $('.navbar').removeClass("sticky");
+        }
+        if(this.scroll> 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+
+        }
+    });
+
+    
+    
+    // toggle menu/navbar script 
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+
+    });
+});
